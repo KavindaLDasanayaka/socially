3c41be93218e59cbdc119d874a527f2ed1fbd910
KavindaLDasanayaka <kavindadasanayaka55@gmail.com>
2025-06-05 23:55:09 +0530

    dtend

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -12,7 +11,7 @@ void main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  // await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: ".env");
   // await FirebaseAppCheck.instance.activate(
   //   androidProvider: AndroidProvider.debug,
   //   appleProvider: AppleProvider.debug,
--- lib/views/auth_views/register.dart
+++ lib/views/auth_views/register.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:image_picker/image_picker.dart';
