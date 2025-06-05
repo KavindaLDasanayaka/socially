718c112f3aeb24e52da417e64d4b871cfb9af1b7
KavindaLDasanayaka <kavindadasanayaka55@gmail.com>
2025-06-05 23:54:35 +0530

    Update user_storage.dart

--- lib/services/storage/user_storage.dart
+++ lib/services/storage/user_storage.dart
@@ -1,25 +1,33 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class UserStorage {
+  final String _cloudName = dotenv.env['CLOUDINARY_CLOUD_NAME'] ?? "";
   // Upload image to Cloudinary
   Future<String?> uploadImageToCloudinary(File imageFile) async {
-    const cloudName = 'dd6afzaus';
-    const uploadPreset = 'profile-pictures';
-    const uploadUrl = 'https://api.cloudinary.com/v1_1/$cloudName/image/upload';
+    try {
+      final String cloudName = _cloudName;
+      const uploadPreset = 'profile-pictures';
+      final String uploadUrl =
+          'https://api.cloudinary.com/v1_1/$cloudName/image/upload';
 
-    final request = http.MultipartRequest('POST', Uri.parse(uploadUrl))
-      ..fields['upload_preset'] = uploadPreset
-      ..files.add(await http.MultipartFile.fromPath('file', imageFile.path));
+      final request = http.MultipartRequest('POST', Uri.parse(uploadUrl))
+        ..fields['upload_preset'] = uploadPreset
+        ..files.add(await http.MultipartFile.fromPath('file', imageFile.path));
 
-    final response = await request.send();
+      final response = await request.send();
 
-    if (response.statusCode == 200) {
-      final resStr = await response.stream.bytesToString();
-      final jsonResponse = json.decode(resStr);
-      return jsonResponse['secure_url'] as String;
+      if (response.statusCode == 200) {
+        final resStr = await response.stream.bytesToString();
+        final jsonResponse = json.decode(resStr);
+        return jsonResponse['secure_url'] as String;
+      }
+    } catch (e) {
+      print("Error saving images:$e");
+      return null;
     }
   }
 }
